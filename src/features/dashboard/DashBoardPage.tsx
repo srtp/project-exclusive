95bcd1a65d93d230f3844e44d05467d0bdd91cad
Petch Settapak <settapak.t@orbitdigital.co.th>
2023-01-22 15:40:21 +0700

    #stucture folder

---
+++ src/features/dashboard/DashBoardPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {}
+
+const DashBoardPage = (props: Props) => (
+    <div>DashBoardPage</div>
+);
+
+export default DashBoardPage;
---
+++ src/features/login/LoginPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {}
+
+const LoginPage = (props: Props) => (
+    <div>LoginPage</div>
+);
+
+export default LoginPage;
